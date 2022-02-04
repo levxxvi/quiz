fb738e3302dd49938cca2ccb9c479d092a9285a5
levxxvi <99051920+levxxvi@users.noreply.github.com>
2022-02-04 14:44:47 -0800

    Updated
    imported Random(). and array size (check this part)

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,33 @@ check if you did it right, need to be able to add questions to the bank*/
 /*thought: array for user input for questions. check how to make methods again. the "variable" in the numOfQs can be
 * taken from array.length. */
 
+/*UI. Ask user if they want to make a questions or take a quiz. If they want to make questions, ask them how many
+questions they want to make. take that number and store it in a variable for a for loop. In the for loop, ask the
+user for questions and answers. If they want to take a quiz, then show them how many questions are in the bank.
+Generate a quiz by taking a random amount of questions from the bank */
+
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         //picking out x # of questions for the quiz
         //change constant to a variable later
-        int numOfQs = (int)(Math.random()*21);
+        int numOfQs = (int)(new Random() *qs.size++);
         System.out.println(numOfQs);
 
         Scanner myObj = new Scanner(System.in);
-        System.out.println("Write a question!");
+        System.out.println("Select Answer");
         String question = myObj.nextLine();
 
         //array for questions
-        String[] qs = {
-
+        String[] qs = {"What year is it?", "Why did the chicken cross the road", "What colour is the lavender shirt",
+                "How many mice does it take to screw a light bulb", "What is the worst letter in the alphabet?"
         };
+
+        //array for answers
+        String[] as1 = {"2022"};
+
 
         //idek atp
         /*class Questions<userQ> {
@@ -31,5 +41,3 @@ public class Main {
             userQ = myObj.nextLine();*/
     }
 }
-
-
