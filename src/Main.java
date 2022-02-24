113a3ad471bf0c7079b892d5a4d1e4afc232c900
levxxvi <99051920+levxxvi@users.noreply.github.com>
2022-02-24 15:01:43 -0800

    Mr. Zaremba fixed up the code!
    -compartmentalize
    -more objects/methods

--- src/Main.java
+++ src/Main.java
@@ -9,55 +9,89 @@ questions they want to make. take that number and store it in a variable for a f
 user for questions and answers. If they want to take a quiz, then show them how many questions are in the bank.
 Generate a quiz by taking a random amount of questions from the bank */
 
-import java.util.Random;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);
+    public static ArrayList<Question> makeQuiz(ArrayList<Question> questions, int size) {
+        ArrayList<Question> quiz = new ArrayList<>();
+        for (int i = 0; i < size; i++){
+            int newQ = (int)(Math.random() * questions.size());
+            Question ques = questions.get(newQ);
+            while (!questions.contains(ques)) {
+                newQ = (int)(Math.random() * questions.size());
+                ques = questions.get(newQ);
+            }
+            quiz.add(ques);
+        }
+        return quiz;
+    }
 
+    public static void main(String[] args) {
+        ArrayList<Question> questions = new ArrayList<>();
+        ArrayList<Question> quiz = new ArrayList<>();
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Lets make a question");
+        System.out.println("Write in a question");
+        String q = myObj.nextLine();
+        System.out.println("What is the answer");
+        String a = myObj.nextLine();
+        questions.add(new Question(q, a));
+
+        System.out.println("How big of a quiz do you want?");
+        int qSize = myObj.nextInt();
+        quiz = makeQuiz(questions, qSize);
+        runQuiz(quiz);
+    }
+
+    private static void runQuiz(ArrayList<Question> quiz) {
+        for (Question q : quiz) {
+            System.out.println(q.question);
+            
+            System.out.println(q.answer);
+        }
+    }
         //array for questions
-        String[] questions = {"What year is it?",
+        /*String[] questions = {"What year is it?",
                 "Why did the chicken cross the road?",
                 "What colour is the lavender shirt?",
                 "How many mice does it take to screw a light bulb?",
                 "What is the worst letter in the alphabet?"
         };
-
+        //array for questions
         String[] answers = {"2022",
                 "To go to the idiot's house... Knock Knock.",
-
+                "PINK!",
+                "Trick Question. Mice don't have good eyesight.",
+                "W"
         };
+        //mark counter
+        int mark = 0;
 
         //picking out x # of questions for the quiz
         int numOfQs = (int)(Math.random() * questions.length);
+        System.out.println("Welcome, dear quiz taker, please be warned that this is an extremely" +
+                " stupid quiz, so don't expect to receive fair questions");
         System.out.println("There are " + questions.length + " questions in the question bank.");
         System.out.println("You will be quizzed " + numOfQs + " questions.");
 
         for (int i = 0; i < numOfQs; i++){
             int newQ = (int)(Math.random() * questions.length);
+            int[] usedQ = {};
+            if (newQ ){}
             System.out.println(questions[newQ]);
             String userAnswer;
             userAnswer = myObj.nextLine();
-            if(userAnswer == answers[newQ]){
+            if(userAnswer.equals(answers[newQ]) == true){
                 System.out.println("WOW! I was NOT expecting you to get that right!");
+                mark++;
+            }
+            else{
+                System.out.println("Unfortunately, that was wrong. The correct answer is \"" + answers[newQ] + "\"");
             }
         }
 
-        /*Scanner myObj = new Scanner(System.in);
-        System.out.println("Select Answer");
-        String question = myObj.nextLine();*/
+        System.out.println("That was the end of the quiz! You got " + mark + " out of " + numOfQs);
 
-        //array for answers
-        /*String[] as1 = {"2022"};*/
-
-
-        //idek atp
-        /*class Questions<userQ> {
-            //asking user for questions (to put into array)
-            Scanner myObj = new Scanner(System.in);
-            System.out.println("Write a question!");
-            String userQ;
-            userQ = myObj.nextLine();*/
-    }
+         */
 }
--- src/Question.java
+++ src/Question.java
@@ -1,6 +1,20 @@
-/*import java.util.ArrayList;
-
 public class Question {
-    ArrayList<String> questions = new ArrayList<String>();
-    questions.add("What year is it");
-}*/
+    String question;
+    String answer;
+
+    public Question(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Question) {
+            Question other = (Question) obj;
+            if (this.question.equals(other.question)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
